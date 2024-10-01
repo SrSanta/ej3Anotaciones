f43544c7f11040969744adc3dcf16e60499a5234
Andrés <andrescondegarcia11@gmail.com>
2024-10-01 17:22:50 +0200

    ej3 hacio perfe

--- src/AgendaSemana.java
+++ src/AgendaSemana.java
@@ -46,6 +46,6 @@ public class AgendaSemana {
     public String toString() {
         return "AgendaSemana{" +
                 "tareas=" + tareaList +
-                '}' + tareaList.size();
+                '}';
     }
 }
